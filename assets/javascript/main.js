e228dddd3de2c602caf2c5824a18dcd3f1051b40
skanakia <skanakia1@gmail.com>
2018-05-30 12:38:52 -0400

    added popup bubble

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,34 @@
 
 $(document).ready(function () {
     $('.parallax').parallax();
+    $('.tap-target').tapTarget();
+    $('.tap-target').tapTarget('open');
+
+
+    $("#menu").on("click", function () {
+        $(this).css("z-index", "1000");
+        $('.tap-target').css("z-index", "501");
+        if ($(".tap-target").isOpen !== true) {
+            $('.tap-target').tapTarget();
+            $('.tap-target').tapTarget('open');
+            $(this).css("visibility", "visible")
+        } else {
+            $('.tap-target').tapTarget();
+            $('.tap-target').tapTarget('close');
+        }
+
+    });
+
+    $('html').click(function() {
+        if ($(".tap-target").isOpen !== true) {
+            $('.tap-target').tapTarget();
+            $('.tap-target').tapTarget('open');
+            $(this).css("visibility", "visible")
+        } else {
+            $('.tap-target').tapTarget();
+            $('.tap-target').tapTarget('close');
+        }
+
+    });
 });
+
